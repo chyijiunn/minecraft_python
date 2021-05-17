cb2c1814b2b7fe56ae22e43782d037cba591c4cb
chyijiunn <chyijiunn@gmail.com>
2021-05-17 14:00:49 +0800

    fix copier

--- 09_project/04_copy/05_tiles_data_selecter_copier.py
+++ 09_project/04_copy/05_tiles_data_selecter_copier.py
@@ -5,9 +5,21 @@ input('copy startpoint , press Enter')
 xs,ys,zs = mc.player.getPos()
 input('copy endpoint , press Enter')
 xe,ye,ze = mc.player.getPos()
-for i in range(int(xs),int(xe)+1,1):
-    for j in range(int(ys),int(ye)+1,1):
-        for k in range(int(zs),int(ze)+1,1):
+ds_x = int(xe-xs)
+ds_y = int(ye-ys)
+ds_z = int(ze-zs)
+if ds_x > 0:
+    count_x = 1
+else :count_x = -1
+if ds_y > 0:
+    count_y = 1
+else :count_y = -1
+if ds_z > 0:
+    count_z = 1
+else :count_z = -1
+for i in range(0 , ds_x ,count_x):
+    for j in range(0 , ds_y ,count_y):
+        for k in range(0 , ds_z ,count_z):
             axis = [i,j,k]
             block_0 =str(axis+list(mc.getBlockWithData(xs+i,ys+j,zs+k)))
             block_1=''.join(block_0.split(' '))#remove space
