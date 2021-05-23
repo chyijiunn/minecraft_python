79e6f309042712547142f8f94f58981fc5a9822c
chyijiunn <chyijiunn@gmail.com>
2021-05-23 10:57:14 +0800

    modified hodoukan

--- 09_project/02_hodoukan/21_hodocan_button.py
+++ 09_project/02_hodoukan/21_hodocan_button.py
@@ -4,7 +4,7 @@ from time import sleep
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setup(12,GPIO.IN)
-
+x,y,z = mc.player.getPos()
 def hodocan(x,y,z,block,sleep_time,attack_range,start_point):
     for i in range(attack_range):
         mc.setBlock(x,y,z+i+start_point,block)
@@ -28,10 +28,6 @@ def hodocan(x,y,z,block,sleep_time,attack_range,start_point):
     mc.setBlock(x-attack_range-start_point,y,z,0)
 
 while True:
-    input_value = GPIO.input(12)
-    if input_value == False:
-        print('fire!')
-            x,y,z = mc.player.getPos()
-        hodocan(x,y,z,10,0.05,10,2)
-        while input_value == False:
-            input_value = GPIO.input(12)
+    x,y,z = mc.player.getPos()
+    hodocan(x,y,z,10,0.05,10,2)
+    sleep(0.5)
